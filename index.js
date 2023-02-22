0e5262407b49187ea626996e397660d9fb03e563
Royson Paul <Roysonpaul.Immanuel@valgenesis.com>
2023-02-22 17:33:40 +0530

    changed server code

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
-const express = require('express');
-
-const dotenv = require('dotenv');
-
 const jwt = require('jsonwebtoken');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-let PORT = process.env.PORT || 5000;
+// Secret key for signing JWT token
+const secretKey = 'my_secret_key';
 
-app.listen(PORT, () => {
+// Sample User data for authentication
+const users = [
+  { id: 1, name: 'John Doe', email: 'john.doe@example.com', password: 'password1' },
+  { id: 2, name: 'Jane Doe', email: 'jane.doe@example.com', password: 'password2' },
+];
 
-  console.log(`Server is up and running on ${PORT} ...`);
+// Login route for generating JWT token
+app.post('/login', (req, res) => {
+  // Get user email and password from request body
+  const { email, password } = req.body;
 
+  // Find user by email and password
+  const user = users.find((user) => user.email === email && user.password === password);
+
+  if (!user) {
+    // If user not found, return error response
+    res.status(401).json({ message: 'Invalid email or password' });
+  } else {
+    // If user found, generate JWT token and return success response
+    const token = jwt.sign({ sub: user.id }, secretKey, { expiresIn: '1h' });
+    res.json({ token });
+  }
 });
 
-app.post("/user/generateToken", (req, res) => {
-
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-
-    let data = {
-
-        time: Date(),
-
-        userId: 12,
-
-    }
-
-    const token = jwt.sign(data, jwtSecretKey);
-
-    res.send(token);
-
-});
-
-app.get("/user/validateToken", (req, res) => {
-
-    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-
+// Sample protected route that requires JWT token authentication
+app.get('/protected', (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
     try {
-
-        const token = req.header(tokenHeaderKey);
-
-        const verified = jwt.verify(token, jwtSecretKey);
-
-        if(verified){
-
-            return res.send("Successfully Verified");
-
-        }else{
-
-            return res.status(401).send(error);
-
-        }
-
-    } catch (error) {
-
-        return res.status(401).send(error);
-
+      // Verify JWT token
+      const decoded = jwt.verify(token, secretKey);
+      const userId = decoded.sub;
+      // Find user by id
+      const user = users.find((user) => user.id === userId);
+      if (user) {
+        // If user found, return success response
+        res.json({ message: `Hello, ${user.name}! This is a protected route.` });
+      } else {
+        // If user not found, return error response
+        res.status(401).json({ message: 'Invalid token' });
+      }
+    } catch (err) {
+      // If JWT verification fails, return error response
+      res.status(401).json({ message: 'Invalid token' });
     }
-
+  } else {
+    // If authorization header not found, return error response
+    res.status(401).json({ message: 'Authorization header not found' });
+  }
 });
+
+// Start server
+app.listen(3000, () => console.log('Server started on port 3000'));
---
+++ old_index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+
+const dotenv = require('dotenv');
+
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+dotenv.config();
+
+let PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+  console.log(`Server is up and running on ${PORT} ...`);
+
+});
+
+app.post("/user/generateToken", (req, res) => {
+
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+    let data = {
+
+        time: Date(),
+
+        userId: 12,
+
+    }
+
+    const token = jwt.sign(data, jwtSecretKey);
+
+    res.send(token);
+
+});
+
+app.get("/user/validateToken", (req, res) => {
+
+    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+    try {
+
+        const token = req.header(tokenHeaderKey);
+
+        const verified = jwt.verify(token, jwtSecretKey);
+
+        if(verified){
+
+            return res.send("Successfully Verified");
+
+        }else{
+
+            return res.status(401).send(error);
+
+        }
+
+    } catch (error) {
+
+        return res.status(401).send(error);
+
+    }
+
+});
